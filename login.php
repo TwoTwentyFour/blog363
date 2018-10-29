83d519193ba631357db1394ed1cdd4e7fe43107d
devan <twotwentyfour@protonmail.com>
2018-10-29 14:09:07 -0400

    Started removing the QF2 stuff from the login system.

--- login.php
+++ login.php
@@ -3,24 +3,7 @@
 require('includes/utilities.inc.php');
 require_once('includes/error_handler.inc.php');
 
-set_include_path(get_include_path() . PATH_SEPARATOR . '/usr/local/pear/share/pear');
-require('HTML/QuickForm2.php');
-$form = new HTML_QuickForm2('loginForm');
-
-$email = $form->addElement('text', 'email');
-$email->setLabel('Email Address');
-$email->addFilter('trim');
-$email->addRule('required', 'Please enter an email address.');
-$email->addRule('email', 'Please enter an email address.');
-
-$password = $form->addElement('password','pass');
-$password->setLabel('Password');
-$password->addFilter('trim');
-$password->addRule('required', 'Please enter your password.');
-
-$form->addElement('submit', 'submit', array('value' => 'Login'));
-
-if ($form->validate())
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     $query = 'SELECT id, userType, username, email FROM users WHERE email=:email AND pass=SHA1(:pass)';
     $stmt = $pdo->prepare($query);
@@ -44,8 +27,10 @@ if ($form->validate())
         exit;
     }
 }
-
-$pageTitle = 'Login';
-include('includes/header.inc.php');
-include('views/login.view.php');
-include('includes/footer.inc.php');
+else
+{
+    $pageTitle = 'Login';
+    include('includes/header.inc.php');
+    include('views/login.view.php');
+    include('includes/footer.inc.php');
+}
--- views/login.view.php
+++ views/login.view.php
@@ -1,14 +1,13 @@
-<section class="threeColumns">
-    <article class="twoThirds">
-        <h1>Login In</h1>
+<?php
 
-        <?php
-            if ($form->isSubmitted() && $form->validate())
-            {
-                echo '<p class="error">Invalid credentials.</p>';
-            }
-        ?>
-
-        <?php echo $form; ?>
-    </article>
-</section>
+echo '<form class="form-horizontal" action="login.php" method="post">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input class="form-control" type="email" name="email" required/>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input class="form-control" type="password" name="pass" required/>
+            </div>
+            <input type="submit" name="submit" value="Login" />
+        </form>';
