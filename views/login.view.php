f3585cfc8f0bc277408c71d147c8cd79e515b54a
devan <twotwentyfour@protonmail.com>
2018-10-31 22:45:04 -0400

    Palyed around with routing a bit. In the end I'm still serving everything from index.

--- views/login.view.php
+++ views/login.view.php
@@ -1,11 +1,11 @@
 <?php
 
 echo '<form class="form-horizontal" action="login.php" method="post" enctype="multipart/form-data">
-            <div class="form-group">
+            <div class="form-group col-md-6">
                 <label for="email">Email</label>
                 <input class="form-control" type="email" name="email" required/>
             </div>
-            <div class="form-group">
+            <div class="form-group col-md-6">
                 <label for="password">Password</label>
                 <input class="form-control" type="password" name="pass" required/>
             </div>
